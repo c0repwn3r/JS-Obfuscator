ec18ae10e07b88d371a5d77af171b7503ecd869e
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-06-25 11:24:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
-const core = require("@actions/core");
-const github = require("@actions/github");
 const JavaScriptObfuscator = require("javascript-obfuscator");
-const glob = require("glob");
 const fs = require("fs");
 
 try {
-  var path = process.cwd() + core.getInput("file");
+  var path = process.cwd() + "/static/scripts/client.js";
   fs.readFile(path, (err, content) => {
     content = content.toString();
     var obf = JavaScriptObfuscator.obfuscate(content).getObfuscatedCode();
     fs.writeFileSync(path, obfuscatedContent);
   });
 } catch (err) {
-  core.setFailed(err.message);
+  process.exit(0);
 }
